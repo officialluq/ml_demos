fdd36572b58361ac4d4ec5040a4e06780a150ef3
officialluq <lukaszprzybylik@gmail.com>
2024-05-12 17:47:47 +0200

    more tutorials

--- knn/knn.py
+++ knn/knn.py
@@ -1,54 +1,42 @@
 import numpy as np
 class KNearestNeihgbor:
-
-
-    # k to liczba najbliższych sąsiadów
     def __init__( self, k ):
-
         self.k = k      
         pass
 
-
-    def Train( self, Dane, EtykietyDanych ):
-
-        self.Dane = Dane
-        self.EtykietyDanych = EtykietyDanych
+    def Train( self, data, labels ):
+        self.data = data
+        self.labels = labels
         pass
 
-
-    # Znajdź etykiety do k najlbiższych sąsiadów - to prawdopodobnie jest klasa danej testowej
-    def Predict( self, DanaTestowa ):
-
+    def Predict( self, data ):
         # Najpierw musimy policzyć odległości - to jest kłopotliwy krok w metodzie kNN
-        dystanse = self.ComputeDistances( DanaTestowa )
+        distance = self.ComputeDistances( data )
 
         # Teraz musimy znaleźć najbliższych sąsiadów, a w zasadzie interesują nas ich etykiety
 
         # argsort sortuje, ale zwraca indeksy (a nie same wartości) najmniejszych obiektów
-        indeksy_najbliższych_sąsiadów = np.argsort( dystanse )
+        knn_indexes = np.argsort( distance )
 
         # znajdźmy jakie są to etykiety
-        k_najbliższych_klas = self.EtykietyDanych[ indeksy_najbliższych_sąsiadów[ : self.k ] ].astype( int )
+        knn_classes = self.labels[ knn_indexes[ : self.k ] ].astype( int )
 
         # w końcu szukamy dominującej klasy w zbiorze k etykiet
-        klasa_danej_testowej = np.argmax( np.bincount( k_najbliższych_klas ) )
+        classes = np.argmax( np.bincount( knn_classes ) )
 
-        return klasa_danej_testowej
+        return 0
     
-
-
-
     # Policz odległości wg pewnej miary odległości właściwej dla tych danych
-    def ComputeDistances( self, DanaTestowa ):
+    def ComputeDistances( self, test_data ):
 
         # self.Dane to tablica (array) o liczbie wierszych == liczbie danych
         # rozmiar każdej danej to liczba kolumn
-        liczba_danych = self.Dane.shape[ 0 ]
-        wymiar_danych = self.Dane.shape[ 1 ]
+        data_count = self.data.shape[ 0 ]
+        wymiar_danych = self.data.shape[ 1 ]
 
         # wymiary muszą się zgadzać
-        assert( DanaTestowa.shape[ 0 ] == wymiar_danych )
+        assert( test_data.shape[ 0 ] == wymiar_danych )
 
         #dist = [ np.linalg.norm( self.Dane[ i ] - DanaTestowa ) for i in range( liczba_danych ) ]
         #return np.array( dist )
-        return np.linalg.norm( self.Dane - DanaTestowa, axis = 1 )        # ord = 2
+        return np.linalg.norm( self.data - test_data, axis = 1 )        # ord = 2
